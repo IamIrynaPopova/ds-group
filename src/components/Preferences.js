8c3233255cd90673730a19f0ff4f803659c2f916
Iryna Popova <popovaira969@gmail.com>
2024-11-01 01:04:45 +0200

    added preferences

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,7 +1,34 @@
 import React from "react";
+import translations from "../data/translations.json";
 
-const Preferences = () => {
-  return <section className="container"></section>;
+const Preferences = ({ language }) => {
+  return (
+    <section className="preferences container">
+      <h2 className="preferences__title">
+        {translations[language].preferences.title}
+      </h2>
+      <ul className="preferences__list">
+        <li className="preferences__item">
+          {translations[language].preferences.preference1}
+        </li>
+        <li className="preferences__item">
+          {translations[language].preferences.preference2}
+        </li>
+        <li className="preferences__item">
+          {translations[language].preferences.preference3}
+        </li>
+        <li className="preferences__item">
+          {translations[language].preferences.preference4}
+        </li>
+        <li className="preferences__item">
+          {translations[language].preferences.preference5}
+        </li>
+        <li className="preferences__item">
+          {translations[language].preferences.preference6}
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default Preferences;
