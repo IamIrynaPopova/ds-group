a33ce2ae0f5d52a3dcc64545459495bcd237ebfd
Iryna Popova <popovaira969@gmail.com>
2024-11-13 01:06:51 +0200

    added time table

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import times from "../data/timetable.json";
 import translations from "../data/translations.json";
+import sprite from "../images/sprite/sprite.svg";
 
 const Timetable = ({ language }) => {
-  return <section className="container" id="time_table"></section>;
+  const [openTable, setOpenTable] = useState(null);
+
+  const toggleTable = (index) => {
+    setOpenTable(openTable === index ? null : index);
+  };
+
+  return (
+    <section className="container time" id="time_table">
+      <h2 className="teams__title">
+        {translations[language].time_table.title}
+      </h2>
+      <p className="teams__subtitle">
+        {translations[language].time_table.teams}
+      </p>
+      <ul className="time__list">
+        {times.map((item, index) => (
+          <li key={index} className="time__item">
+            <div
+              className="time__header"
+              onClick={() => toggleTable(index)}
+              style={{
+                borderBottom: openTable === index && "none",
+              }}
+            >
+              <p> {item[language].team}</p>
+              <svg
+                className={`time__header-svg ${
+                  openTable === index ? "arrow-top" : "arrow-bottom"
+                }`}
+              >
+                <use href={`${sprite}#arrow-bottom`}></use>
+              </svg>
+            </div>
+            {openTable === index && (
+              <div className="time__content" style={{ borderTop: "none" }}>
+                <p>
+                  {item[language].day1}
+                  <span> {item[language].time1}</span>
+                </p>
+                <p>
+                  {item[language].day2}
+                  <span> {item[language].time2}</span>
+                </p>
+                <p>
+                  {item[language].day3}
+                  <span> {item[language].time3}</span>
+                </p>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Timetable;
