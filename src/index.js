29440bb53247899861cc797a7bb97c69bfcd678c
Iryna Popova <popovaira969@gmail.com>
2024-12-12 01:21:26 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./components/App";
 import css from "./styles/main.scss";
@@ -19,8 +19,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const basename = process.env.NODE_ENV === "production" ? "/ds-group" : "/";
 root.render(
   <StrictMode>
-    <HashRouter  basename={basename}>
+    <BrowserRouter  basename={basename}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
