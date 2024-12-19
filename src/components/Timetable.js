69a11136c23cfb5466a21cc192b0b63bd6fb47d4
Iryna Popova <popovaira969@gmail.com>
2024-12-19 20:18:58 +0200

    added steles time table

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -1,14 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import times from "../data/timetable.json";
 import translations from "../data/translations.json";
 import sprite from "../images/sprite/sprite.svg";
 
 const Timetable = ({ language }) => {
   const [openTable, setOpenTable] = useState(null);
-
+  const [isMobile, setIsMobile] = useState(true);
   const toggleTable = (index) => {
     setOpenTable(openTable === index ? null : index);
   };
+
+  useEffect(() => {
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIsMobile();
+
+    window.addEventListener("resize", checkIsMobile);
+
+    return () => window.removeEventListener("resize", checkIsMobile);
+  }, []);
+  if (!isMobile) {
+    return (
+      <section className="time container" id="time_table">
+        <h2 className="teams__title">
+          {translations[language].time_table.title}
+        </h2>
+
+        <ul className="teams__weekdays-list">
+          <li className="teams__weekdays-item">
+            {translations[language].time_table.teams}
+          </li>
+          <li className="teams__weekdays-item">
+            {translations[language].weekdays.MO}
+          </li>
+          <li className="teams__weekdays-item">
+            {translations[language].weekdays.TU}
+          </li>
+          <li className="teams__weekdays-item">
+            {translations[language].weekdays.WE}
+          </li>
+          <li className="teams__weekdays-item">
+            {translations[language].weekdays.TH}
+          </li>
+          <li className="teams__weekdays-item">
+            {translations[language].weekdays.FR}
+          </li>
+        </ul>
+        {times.map((item, index) => (
+          <ul className="teams__weekdays-table" key={index}>
+            <li className="teams__weekdays-day">{item[language].team}</li>
+            {[
+              { en: "MO", ua: "ПН" },
+              { en: "TU", ua: "ВТ" },
+              { en: "WE", ua: "СР" },
+              { en: "TH", ua: "ЧТ" },
+              { en: "FR", ua: "ПТ" },
+            ].map((day, dayIndex) => {
+              const matchedDay = item[language].days.find(
+                (weekday) => weekday.day === day[language]
+              );
+
+              return (
+                <li className="teams__weekdays-day" key={dayIndex}>
+                  {matchedDay ? matchedDay.time : " "}
+                </li>
+              );
+            })}
+          </ul>
+        ))}
+      </section>
+    );
+  }
 
   return (
     <section className="time container" id="time_table">
@@ -23,36 +87,36 @@ const Timetable = ({ language }) => {
           <li key={index} className="time__item">
             <div
               className="time__header"
-              onClick={() => toggleTable(index)}
+              onClick={() => isMobile && toggleTable(index)}
               style={{
                 borderBottom: openTable === index && "none",
               }}
             >
               <p> {item[language].team}</p>
-              <svg
-                className={`time__header-svg ${
-                  openTable === index ? "arrow-top" : "arrow-bottom"
-                }`}
-              >
-                <use href={`${sprite}#arrow-bottom`}></use>
-              </svg>
+              {isMobile && (
+                <svg
+                  className={`time__header-svg ${
+                    openTable === index ? "arrow-top" : "arrow-bottom"
+                  }`}
+                >
+                  <use href={`${sprite}#arrow-bottom`}></use>
+                </svg>
+              )}
             </div>
-            {openTable === index && (
-              <div className="time__content" style={{ borderTop: "none" }}>
-                <p>
-                  {item[language].day1}
-                  <span> {item[language].time1}</span>
+            <div
+              className="time__content"
+              style={{
+                borderTop: "none",
+                display: !isMobile || openTable === index ? "block" : "none",
+              }}
+            >
+              {item[language].days.map((weekday, index) => (
+                <p key={index}>
+                  {weekday.day}
+                  <span> {weekday.time}</span>
                 </p>
-                <p>
-                  {item[language].day2}
-                  <span> {item[language].time2}</span>
-                </p>
-                <p>
-                  {item[language].day3}
-                  <span> {item[language].time3}</span>
-                </p>
-              </div>
-            )}
+              ))}
+            </div>
           </li>
         ))}
       </ul>
